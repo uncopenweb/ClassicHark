29030b0dbcc2a203842840cc2b0540a2fe9a99d2
Robert Dallara <robertdallara@nc.rr.com>
2011-08-02 02:46:20 -0400

    Fixed references to soundModule.

--- HarkTheSound/widgets/reactionGameEngine.js
+++ HarkTheSound/widgets/reactionGameEngine.js
@@ -107,7 +107,7 @@ dojo.declare('widgets.reactionGameEngine', [dijit._Widget, dijit._Templated], {
     // pops up game "instructions". 
     // @todo: should let user know what the good and bad sounds are
     _doInstructions: function() { 
-		this.soundModule.getAudio().setProperty({name : 'volume', value : soundModule.masterVolume*soundModule.speechVolume, immediate : true});
+		this.soundModule.getAudio().setProperty({name : 'volume', value : this.soundModule.masterVolume*this.soundModule.speechVolume, immediate : true});
         this.soundModule.getAudio().say({text: this.instructions}).callBefore(dojo.hitch(this, function() {  
             this._loadingDialog._alreadyInitialized=true;    //so that .hide will have effect   
             this._loadingDialog.hide();
@@ -505,7 +505,7 @@ dojo.declare('widgets.reactionGameEngine', [dijit._Widget, dijit._Templated], {
 		if(conveyPaused)
 		{
 			var pauseMessage = this._oneOf(this.pauseMessages);
-			this.soundModule.getAudio().setProperty({name : 'volume', value : soundModule.masterVolume*soundModule.speechVolume, immediate : true});
+			this.soundModule.getAudio().setProperty({name : 'volume', value : this.soundModule.masterVolume*this.soundModule.speechVolume, immediate : true});
 			
 			this.soundModule.getAudio().say({text : pauseMessage}).callBefore(dojo.hitch(this, function() 
 			{
