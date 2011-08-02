5c300a420a713ed17959ee3782a8b603a4e3561c
Robert Dallara <robertdallara@nc.rr.com>
2011-08-02 02:10:48 -0400

    Added sound module back into project.

---
+++ HarkTheSound/widgets/soundModule.js
@@ -0,0 +1,39 @@
+dojo.provide('widgets.soundModule');
+
+dojo.declare('widgets.soundModule', null, 
+{
+	masterVolume: 0,
+	speechVolume: 0,
+	soundVolume: 0,
+	_audio: null,
+	
+    constructor: function(audio) 
+	{
+        this._audio = audio;
+	},
+	
+	getAudio: function()
+	{
+		return this._audio;
+	},
+	
+	//Speaks an utterance over a certain audio channel
+	speak: function(string, audioChannel, shouldStop, afterFunction)
+	{
+		if(shouldStop)
+			this._audio.stop({channel : audioChannel});
+				
+		this._audio.setProperty({name : 'volume', channel : audioChannel, value : this.masterVolume*this.speechVolume, immediate : true});
+		return this._audio.say({text : string, channel : audioChannel}).callAfter(afterFunction);
+	},
+
+	//Plays a certain sound over a certain audio channel
+	playSound: function(urlString, audioChannel, shouldStop, afterFunction)
+	{
+		if(shouldStop)
+			this._audio.stop({channel : audioChannel});
+			
+		this._audio.setProperty({name : 'volume', channel : audioChannel, value : this.masterVolume*this.soundVolume, immediate : true});
+		return this._audio.play({url : urlString, channel : audioChannel}).callAfter(afterFunction);
+	}
+});
