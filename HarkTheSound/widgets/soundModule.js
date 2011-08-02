d93d5739f32d4836f9d1fd3b832ad3bbbeca371b
Robert Dallara <robertdallara@nc.rr.com>
2011-08-02 19:29:32 -0400

    Added logged messages to debugging console to fix volume bug at beginning of "Coins and Barrels."

--- HarkTheSound/widgets/soundModule.js
+++ HarkTheSound/widgets/soundModule.js
@@ -19,17 +19,17 @@ dojo.declare('widgets.soundModule', null,
 	
 	//Speaks an utterance over a certain audio channel
 	speak: function(string, audioChannel, shouldStop, afterFunction)
-	{
+	{console.log("Speaking... String: "+string+", Audio Channel: "+audioChannel);
 		if(shouldStop)
 			this._audio.stop({channel : audioChannel});
-				
+		
 		this._audio.setProperty({name : 'volume', channel : audioChannel, value : this.masterVolume*this.speechVolume, immediate : true});
 		return this._audio.say({text : string, channel : audioChannel}).callAfter(afterFunction);
 	},
 
 	//Plays a certain sound over a certain audio channel
 	playSound: function(urlString, audioChannel, shouldStop, afterFunction)
-	{
+	{console.log("Playing... Source: "+urlString+", Audio Channel: "+audioChannel);
 		if(shouldStop)
 			this._audio.stop({channel : audioChannel});
 			
