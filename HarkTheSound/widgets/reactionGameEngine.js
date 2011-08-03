c3c1fda9742ae03516a7ca8289a68438671b9672
Robert Dallara <robertdallara@nc.rr.com>
2011-08-02 22:11:00 -0400

    Tried to have bad sound play in "after function" of speech about it.

--- HarkTheSound/widgets/reactionGameEngine.js
+++ HarkTheSound/widgets/reactionGameEngine.js
@@ -114,7 +114,7 @@ dojo.declare('widgets.reactionGameEngine', [dijit._Widget, dijit._Templated], {
         this.soundModule.getAudio().say({text: this.instructions}).callBefore(dojo.hitch(this, function() {  
 			this.soundModule.getAudio().setProperty({name : 'volume', value : this.soundModule.masterVolume*this.soundModule.speechVolume, immediate : true}); //Make sure volume of audio channel is set before sound begins playing
             this.playingBeginningSpeech=true;
-			console.log("Playing Beginning Speech: "+this.playingBeginningSpeech);
+			
 			this._loadingDialog._alreadyInitialized=true;    //so that .hide will have effect   
             this._loadingDialog.hide();
             var instructionsDialog = this._showDialog("Instructions", this.instructions);      
@@ -156,13 +156,15 @@ dojo.declare('widgets.reactionGameEngine', [dijit._Widget, dijit._Templated], {
                     else{
                         var sound = badSoundsCopy.pop();
 						
-						this.soundModule.speak("Here's the next bad sound.", 'default', false, function(){});
-						this.soundModule.playSound(sound, 'default', false, function()
+						this.soundModule.speak("Here's the next bad sound.", 'default', false, function()
 						{
-							if(badSoundsCopy.length<1)
+							this.soundModule.playSound(sound, 'default', false, function()
 							{
-								this.playingBeginningSpeech=false;console.log("Playing Beginning Speech: "+this.playingBeginningSpeech);
-							}
+								if(badSoundsCopy.length<1)
+								{
+									this.playingBeginningSpeech=false;console.log("Playing Beginning Speech: "+this.playingBeginningSpeech);
+								}
+							});
 						});						
                     }
                 }
